8a12084834be5c99b443162b3484ac39af45ded8
GabrielaGuedes <gabriela.guedes.gmg@gmail.com>
2018-05-26 22:15:05 -0300

    Update Mestre.java

--- Mestre.java
+++ Mestre.java
@@ -4,48 +4,37 @@ public class Mestre extends Jedi{
 	private boolean esquivo;
 	private boolean videncia;
 	private boolean imortalidade;
-	public Habilidade forcePush = new Habilidade("force push", "force", 8.0, ((long)3.0));
-	public Habilidade saberSlash = new Habilidade("saber slash", "saber", 12, ((long)2.0));
-	public Habilidade hurlSaber = new Habilidade("hurl saber", "force", 16, ((long)3.0));
-	public Habilidade counterattack = new Habilidade("counterattack", "saber", 5.0, ((long)1.0));
-	public Habilidade esquiva = new Habilidade("esquiva", "force", 0, ((long)0.5));
-	private Habilidade[] prioridades = {esquiva, hurlSaber, forcePush, saberSlash, counterattack,};
+	public Habilidade forcePush = new Habilidade("force push", "force", 8.0, 4);
+	public Habilidade saberSlash = new Habilidade("saber slash", "saber", 12, 2);
+	public Habilidade hurlSaber = new Habilidade("hurl saber", "force", 16, 4);
+	public Habilidade counterattack = new Habilidade("counterattack", "saber", 5.0, 2);
+	public Habilidade esquiva = new Habilidade("esquiva", "force", 0, 0);
+	private Habilidade[] prioridades = {hurlSaber, forcePush, saberSlash, counterattack};
 	
-	Mestre(String nome, int percentualPaz){
+	Mestre(String nome, double percentualPaz){
 		this.setNome(nome);
 		this.setPercentualPaz(percentualPaz);
+		this.setHP(100.0);
+	}
+	public Habilidade[] getHabilidades() {
+		return prioridades;
 	}
 	public void setPrioridades(Habilidade[] prioridades) {
 		this.prioridades = prioridades;
 	}
 	public Habilidade usarHabilidade() {
-		return this.prioridades[0];
+		int i = 0;
+		while (i < this.prioridades.length && this.prioridades[i].getCoolOff() > 0)
+			i++;
+		this.prioridades[i].setCoolOff(this.prioridades[i].getCooldown());
+		return this.prioridades[i];
 	}
 	public boolean isEsquivo() {
 		return esquivo;
 	}
 	public void setEsquivo(boolean esquivo) {
 		this.esquivo = esquivo;
-		if(!esquivo) {
-			Habilidade[] p = {counterattack, esquiva, hurlSaber, forcePush, saberSlash};
-			setPrioridades(p);
-		} else {
-			Habilidade[] p = {hurlSaber, forcePush, saberSlash, counterattack, esquiva};
-			setPrioridades(p);
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}	
 	public boolean isVidencia() {
 		return videncia;
 	}
