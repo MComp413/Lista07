b2e86d146a72670faf34b5bb1fb1518636a5eed6
GabrielaGuedes <gabriela.guedes.gmg@gmail.com>
2018-05-26 22:14:34 -0300

    Update Lorde.java

--- Lorde.java
+++ Lorde.java
@@ -3,38 +3,38 @@ package EP;
 public class Lorde extends Sith{
 	private Aprendiz pupilo;
 	private boolean esquivo;
-	public Habilidade forceChoke = new Habilidade("force choke", "force", 10, ((long)6.0));
-	public Habilidade saberSlash = new Habilidade("saber slash", "saber", 14, ((long)2.0));
-	public Habilidade forceShock = new Habilidade("force shock", "force", 12, ((long)5.0));
-	public Habilidade counterattack = new Habilidade("counterattack", "saber", 5.0, ((long)1.0));
-	public Habilidade esquiva = new Habilidade("esquiva", "force", 0, ((long)2.5));
-	private Habilidade[] prioridades = {esquiva, forceShock, forceChoke, saberSlash, counterattack};
+	public Habilidade forceChoke = new Habilidade("force choke", "force", 10.0, 6);
+	public Habilidade saberSlash = new Habilidade("saber slash", "saber", 14.0, 2);
+	public Habilidade forceShock = new Habilidade("force shock", "force", 12.0, 4);
+	public Habilidade counterattack = new Habilidade("counterattack", "saber", 5.0, 2);
+	public Habilidade esquiva = new Habilidade("esquiva", "force", 0, 0);
+	private Habilidade[] prioridades = {forceShock, forceChoke, saberSlash, counterattack};
 	
-	Lorde(String nome, int percentualRaiva){
+	Lorde(String nome, double percentualRaiva){
 		this.setNome(nome);
 		this.setPercentualRaiva(percentualRaiva);
+		this.setHP(100.0);
 	}
 	
+	public Habilidade[] getHabilidades() {
+		return prioridades;
+	}
 	public void setPrioridades(Habilidade[] prioridades) {
 		this.prioridades = prioridades;
 	}
 	public Habilidade usarHabilidade() {
-		return this.prioridades[0];
+		int i = 0;
+		while (i < this.prioridades.length && this.prioridades[i].getCoolOff() > 0)
+			i++;
+		this.prioridades[i].setCoolOff(this.prioridades[i].getCooldown());
+		return this.prioridades[i];
 	}
 	public boolean isEsquivo() {
 		return esquivo;
 	}
 	public void setEsquivo(boolean esquivo) {
 		this.esquivo = esquivo;
-		if(!esquivo) {
-			Habilidade[] p = {counterattack, esquiva, forceShock, forceChoke, saberSlash};
-			setPrioridades(p);
-		} else {
-			Habilidade[] p = {forceShock, forceChoke, saberSlash, counterattack, esquiva};
-			setPrioridades(p);
-		}
 	}
-	
 	
 	public Aprendiz getPupilo() {
 		return pupilo;
